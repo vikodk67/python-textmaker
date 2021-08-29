db554f47d25ae28be2cd806e64312a830d6d3f37
M R V I K O <vikodk67@gmail.com>
2021-08-29 12:02:05 +0700

    Update __init__.py

--- lib/textmaker/__init__.py
+++ lib/textmaker/__init__.py
@@ -8,7 +8,7 @@ class tulis:
         self.text = listOrText
         self.output = []
     def tulis(self):
-        img, font, kata, tempkata=Image.open("lib/textmaker/pemandangan.jpg"), ImageFont.truetype("lib/textmaker/font.otf",55),'',''
+        img, font, kata, tempkata=Image.open("lib/textmaker/pemandangan.jpg"), ImageFont.truetype("lib/textmaker/font.otf",25),'',''
         draw=ImageDraw.Draw(img)
         if type(self.text) is not list:
             self.output=[]
