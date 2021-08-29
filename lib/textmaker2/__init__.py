65b30f645f412ce03746724ef191c0fbe41e26db
M R V I K O <vikodk67@gmail.com>
2021-08-29 14:18:38 +0700

    Update __init__.py

--- lib/textmaker2/__init__.py
+++ lib/textmaker2/__init__.py
@@ -8,7 +8,7 @@ class tulis:
         self.text = listOrText
         self.output = []
     def tulis(self):
-        img, font, kata, tempkata=Image.open("lib/textmaker2/gambar.jpg"), ImageFont.truetype("lib/textmaker2/Lato-Black.ttf",26),'',''
+        img, font, kata, tempkata=Image.open("lib/textmaker2/gambar.jpg"), ImageFont.truetype("lib/textmaker2/Lato-Black.ttf",20),'',''
         draw=ImageDraw.Draw(img)
         if type(self.text) is not list:
             self.output=[]
@@ -22,7 +22,7 @@ class tulis:
             spliter=kata.split("\n")
         else:
             spliter=self.text
-        line=400
+        line=450
         for i in spliter[:56]:
             draw.text((50, int(line)), i, font=font, fill=("white")) #selisih = Line
             line+=27 + 19
