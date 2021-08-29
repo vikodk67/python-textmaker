49df64aa2c3a006158242c2fbdc37318ac4a14be
M R V I K O <vikodk67@gmail.com>
2021-08-29 15:25:12 +0700

    Update __init__.py

--- lib/textmaker3/__init__.py
+++ lib/textmaker3/__init__.py
@@ -13,7 +13,7 @@ class tulis:
         if type(self.text) is not list:
             self.output=[]
             for i in self.text:
-                if draw.textsize(tempkata, font)[0] < 830:
+                if draw.textsize(tempkata, font)[0] < 940:
                     tempkata+=i
                 else:
                     kata, tempkata=kata+'%s\n'%tempkata, i
