252ddf3d28367fe7e56643df44b553ff892c1f69
M R V I K O <vikodk67@gmail.com>
2021-11-14 20:21:24 +0700

    Update __init__.py

--- lib/textmaker2/__init__.py
+++ lib/textmaker2/__init__.py
@@ -27,7 +27,7 @@ class tulis:
         line=270
         for i in spliter[:56]:
             draw.text((30, int(line)), i, font=font, fill=("white")) #selisih = Line
-            line+=21 + 30
+            line+=20 + 21
         self.output.append(img)
         if len(spliter) > 80:
             self.output+=tulis(spliter[80:]).tulis()
